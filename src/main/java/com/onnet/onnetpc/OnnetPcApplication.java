27683e00b421f6a392993dc019e353588fabc00e
Chu Nguyen Quang Thai <1955010189@kientruchanoi.edu.vn>
2026-03-12 01:14:35 +0700

    first commit

---
+++ src/main/java/com/onnet/onnetpc/OnnetPcApplication.java
@@ -0,0 +1,13 @@
+package com.onnet.onnetpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnnetPcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnnetPcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onnet/onnetpc/OnnetPcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onnet.onnetpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnnetPcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
